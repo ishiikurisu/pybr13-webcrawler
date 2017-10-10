5aa213d3a2f379d247458ef14e94197718e1eafe
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-10-10 10:30:37 -0300

    Finding links

--- main.py
+++ main.py
@@ -9,7 +9,16 @@ def main():
     # Escolhendo um navegador
     driver = webdriver.Firefox() # ou webdrive.Chrome(), por exemplo
     driver.get('http://www.imdb.com/chart/top')
-    time.sleep(5)
+
+    # Manipulando os elementos
+    dado = driver.find_element_by_id('home_img')
+    print(dado)
+
+    mais_dado = driver.find_element_by_css_selector('td.titleColumn a')
+    link = mais_dado.get_attribute("href")
+    print(link)
+
+    # Fechando o driver
     driver.close()
 
 if __name__ == '__main__':
